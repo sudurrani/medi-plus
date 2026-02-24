1036c19af17d7669fd79c1b75b627890e3147a37
Subhan Shah <subhan.shah.bha@outlook.com>
2026-02-24 14:52:36 +0500

    Merge Issue

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { UserLogin } from './auth/login/components/user-login/user-login';
 import { ForgotPassword } from './auth/forgot-password/components/forgot-password/forgot-password';
+import { UserRegistrationComponent } from './auth/register/components/user-registration/user-registration';
 
 export const routes: Routes = [
   { path: 'login', component: UserLogin },
+  { path: 'register', component: UserRegistrationComponent },
   { path: 'forgot-password', component: ForgotPassword },
   { path: '', redirectTo: '/login', pathMatch: 'full' }
 ];
