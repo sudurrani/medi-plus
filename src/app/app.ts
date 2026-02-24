3addf8491ef78f5865799f7d617801fca7783aee
Subhan Shah <subhan.shah.bha@outlook.com>
2026-02-24 14:38:27 +0500

    work is done

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-<<<<<<< HEAD
 import { UserLogin } from './auth/login/components/user-login/user-login';
 import { RouterOutlet } from '@angular/router';
 @Component({
@@ -8,14 +7,5 @@ import { RouterOutlet } from '@angular/router';
   imports: [UserLogin ,  [RouterOutlet],], 
    
   templateUrl: './app.html'
-=======
-import { RouterModule, RouterOutlet } from '@angular/router';
-
-@Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],  
-  templateUrl: './app.html'  
->>>>>>> ca9a3ab4796bfbbe138f55ae8a0eed2a3933a0bd
 })
 export class App {}
