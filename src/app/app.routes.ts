68496939b41fd667892f8aefa9b9aa54df67412b
JunaidAli8BHA <junaid.ali.bha@outlook.com>
2026-02-24 11:20:20 +0500

    push changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { UserRegistrationComponent } from './auth/register/components/user-registration/user-registration';
+import { UserLogin } from './auth/login/components/user-login/user-login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component:  UserLogin },
+  { path: 'register', component: UserRegistrationComponent }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { UserLogin } from './auth/login/components/user-login/user-login';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [UserLogin],   // ✅ YAHAN IMPORT KARNA ZAROORI HAI
+  imports: [UserLogin,RouterOutlet],  
   templateUrl: './app.html'
 })
 export class App {}
