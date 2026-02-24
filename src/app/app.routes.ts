a263cdecc67a6bae489e41596bdd35aa074722ec
Subhan Shah <subhan.shah.bha@outlook.com>
2026-02-24 14:24:03 +0500

    forget password work is done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { UserLogin } from './auth/login/components/user-login/user-login';
+import { ForgotPassword } from './auth/forgot-password/components/forgot-password/forgot-password';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: UserLogin },
+  { path: 'forgot-password', component: ForgotPassword },
+  { path: '', redirectTo: '/login', pathMatch: 'full' }
+];
