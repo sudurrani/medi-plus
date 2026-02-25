59399bb17238a62e1fb62e76b46eb717498cd5b2
Subhan Shah <subhan.shah.bha@outlook.com>
2026-02-25 15:29:03 +0500

    console and alert woek is done

--- src/app/auth/login/components/user-login/user-login.ts
+++ src/app/auth/login/components/user-login/user-login.ts
@@ -2,20 +2,31 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+
 @Component({
   selector: 'app-user-login',
   standalone: true,
-  imports: [CommonModule, FormsModule ,RouterLink ],
-  
+  imports: [CommonModule, FormsModule, RouterLink],
   templateUrl: './user-login.html',
   styleUrls: ['./user-login.css']
 })
 export class UserLogin {
+  
+  loginObj: any = {
+    email: '',
+    password: ''
+  };
 
- 
-   onLogin() {
-  alert('Login button clicked ');
-}
+  onLogin() {
+    
+    console.log("Login Details:", this.loginObj);
+    
+    if(this.loginObj.email && this.loginObj.password) {
+      alert("Logging in with: " + this.loginObj.email);
+    } else {
+      alert("Bhai, pehle email aur password toh likho!");
+    }
+  }
 
   onForgotPassword(event: Event) {
     event.preventDefault();
@@ -26,5 +37,4 @@ export class UserLogin {
     event.preventDefault();
     console.log('Register Now clicked');
   }
-
-} 
+}
